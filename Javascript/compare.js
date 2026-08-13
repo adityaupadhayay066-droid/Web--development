6f4de9a895329153e11680c0a46192d1c2bf4c8f
adityaupadhayay066droid <adityaupadhayay066@gmail.com>
2026-08-13 18:42:03 +0530

    Adding Variables in js

---
+++ Javascript/compare.js
@@ -0,0 +1,16 @@
+let rollno = 100;
+let reg = "100";
+if(rollno==reg){
+    console.log("True")
+}
+else{
+    console.log("False")
+
+}
+
+if(rollno === reg){
+    console.log("true")
+}
+else{
+    console.log("false")
+}
