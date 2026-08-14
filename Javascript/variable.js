2df14a9b54c59e00edaf91d3d8bb1c6c82ff8194
adityaupadhayay066droid <adityaupadhayay066@gmail.com>
2026-08-14 22:57:28 +0530

    Adding variable file in js

---
+++ Javascript/variable.js
@@ -0,0 +1,13 @@
+var a=20;
+console.log(a)
+for(let i=0;i<=1;i++){
+    var num=10;
+}
+console.log(num);
+
+// constant variable 
+const pie =3.14;
+// pie = 3.1;
+console.log(pie);
+let c =null;
+console.log(typeof(c));
